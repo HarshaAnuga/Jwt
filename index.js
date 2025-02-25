418178d5f8781e4857ef6ef621822616a260424e
Harsha Anuga <harshaanuga358@gmail.com>
2025-02-25 16:50:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const jwtPassword = "123456";
+
+const app = express();
+
+const ALL_USERS = [
+  {
+    username: "harkirat@gmail.com",
+    password: "123",
+    name: "harkirat singh",
+  },
+  {
+    username: "raman@gmail.com",
+    password: "123321",
+    name: "Raman singh",
+  },
+  {
+    username: "priya@gmail.com",
+    password: "123321",
+    name: "Priya kumari",
+  },
+];
